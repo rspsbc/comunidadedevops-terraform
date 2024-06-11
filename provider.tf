105ad9bad1808cbe099a5e89d38800dbe284296f
Robson de Souza <rspsbc@gmail.com>
2024-06-11 15:50:33 -0300

    feat(terraform/provider): vpc fix

--- provider.tf
+++ provider.tf
@@ -1,13 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.53.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-rspsbc"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
 }
