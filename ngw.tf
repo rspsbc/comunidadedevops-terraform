832eba344d8a715f251cf6c81b63184cf5de30a4
Robson de Souza <rspsbc@gmail.com>
2024-06-12 20:39:12 -0300

    feat(terraform/comunidadedevops): NGW - Nat-gateway

--- ngw.tf
+++ ngw.tf
@@ -20,7 +20,7 @@ resource "aws_eip" "eks_ngw_eip_1b" {
 
 resource "aws_nat_gateway" "eks_ngw_1a" {
   allocation_id = aws_eip.eks_ngw_eip_1a.id
-  subnet_id     = aws_subnet.eks_subnet_public_1a.id
+  subnet_id     = aws_subnet.eks_subnet_private_1a.id
 
   tags = merge(
     local.tags,
@@ -32,12 +32,44 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
 
 resource "aws_nat_gateway" "eks_ngw_1b" {
   allocation_id = aws_eip.eks_ngw_eip_1b.id
-  subnet_id     = aws_subnet.eks_subnet_public_1b.id
+  subnet_id     = aws_subnet.eks_subnet_private_1b.id
 
   tags = merge(
     local.tags,
     {
       Name = "${var.project_name}-ngw-1b"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1a" {
+  vpc_id = aws_vpc.eks_vcp.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1a.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-priv-route-table-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1b" {
+  vpc_id = aws_vpc.eks_vcp.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-priv-route-table-1b"
     }
   )
 }
