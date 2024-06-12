28fa557e7c203d5097f59ba6ea8703eac7eabeb5
Robson de Souza <rspsbc@gmail.com>
2024-06-12 18:35:19 -0300

    feat(terraform/comunidadedevops): decouple tags, cidrsubnet, tfvars, locals, variables, tags

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  tags = {
+    Department   = "DevOps"
+    Organization = "Infrastrucutre and Operations"
+    Project      = "EKS"
+    Environment  = "Development"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "cidr_block" {
+  type        = string
+  description = "Newrorking CIDR block to be used for the VPC "
+
+}
