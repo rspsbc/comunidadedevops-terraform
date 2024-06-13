af9af56cbf9c63d83ee469e1bff09089b96cc190
Robson de Souza <rspsbc@gmail.com>
2024-06-12 22:14:55 -0300

    feat(terraform/comunidadedevops): cluster, modules, variables

--- modules/cluster/iam.tf
+++ modules/cluster/iam.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   )
 }
 
-resource "aws_iam_role_policy_attachment" "eks-eks_cluster_role_attachment" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 
--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -7,3 +7,13 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to asw resourses"
 }
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "Subnet to create EKs cluster AZ 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "Subnet to create EKs cluster AZ 1b"
+}
