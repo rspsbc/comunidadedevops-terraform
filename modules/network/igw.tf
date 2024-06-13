fcfe56b708358d6ee63b9c659d15f4fed78c1808
Robson de Souza <rspsbc@gmail.com>
2024-06-12 21:13:00 -0300

    refator(terraform/comunidadedevops): terraform modules

--- modules/network/igw.tf
+++ modules/network/igw.tf
@@ -1,7 +1,7 @@
 resource "aws_internet_gateway" "eks_igw" {
   vpc_id = aws_vpc.eks_vcp.id
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}-igw"
       "kubernetes.io/role/elb" = 1
@@ -18,7 +18,7 @@ resource "aws_route_table" "eks_public_route_table" {
   }
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-pub-route-table"
     }
