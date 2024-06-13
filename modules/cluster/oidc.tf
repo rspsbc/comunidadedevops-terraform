e249946efec90db9853de1cc94db9c668e199833
Robson de Souza <rspsbc@gmail.com>
2024-06-13 20:34:26 -0300

    feat(terraform/comunidadedevops): oidc - add tags

--- modules/cluster/oidc.tf
+++ modules/cluster/oidc.tf
@@ -8,4 +8,10 @@ resource "aws_iam_openid_connect_provider" "eks_oidc" {
   ]
   thumbprint_list = data.tls_certificate.oidc_tls_certificate.certificates[*].sha1_fingerprint
   url             = data.tls_certificate.oidc_tls_certificate.url
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-oidc"
+    }
+  )
 }
