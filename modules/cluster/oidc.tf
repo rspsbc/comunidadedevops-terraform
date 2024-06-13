25ef722fd2dbbeb096ce73eb03b8097f9e63f233
Robson de Souza <rspsbc@gmail.com>
2024-06-13 20:32:47 -0300

    feat(terraform/comunidadedevops): oidc - identity provider

---
+++ modules/cluster/oidc.tf
@@ -0,0 +1,11 @@
+data "tls_certificate" "oidc_tls_certificate" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+  thumbprint_list = data.tls_certificate.oidc_tls_certificate.certificates[*].sha1_fingerprint
+  url             = data.tls_certificate.oidc_tls_certificate.url
+}
